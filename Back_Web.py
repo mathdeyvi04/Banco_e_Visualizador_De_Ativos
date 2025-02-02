a4006a44444604233581e9f3599cc34a4718c55a
MathDeyvi <deyvisson.moreno@ime.eb.br>
2025-02-02 11:34:03 -0300

    ADD: Construção da Ideia de Múltiplas Chaves

--- Back_Web.py
+++ Back_Web.py
@@ -1,9 +1,4 @@
-import streamlit as st
 from Get_Trading_Data import *
-
-chave = obter_chave(
-    r"C:\Users\deyvi\Documents\ImperioPy\Aplicativos\Automação\Banco_e_Visualizador_De_Ativos\CHAVE.txt"
-)
 
 
 def obter_ativos_cadastrados() -> list[str]:
@@ -34,65 +29,38 @@ def cadastrar_novo_ativo():
     )
 
     if nome_dado_pelo_usuario != "":
-        pass
 
-        # try:
-        #     resultados_de_busca_de_semelhantes = buscar_ativos_semelhantes(
-        #         nome_dado_pelo_usuario,
-        #         chave
-        #     )
-        #
-        #     st.write(
-        #         resultados_de_busca_de_semelhantes
-        #     )
-        #
-        #     if len(
-        #             resultados_de_busca_de_semelhantes
-        #     ) > 0:
-        #
-        #         nome_das_colunas = (
-        #             "Código",
-        #             "Nome",
-        #             "Tipo do Ativo",
-        #             "Região",
-        #             "Abertura",
-        #             "Fechamento",
-        #             "Fuso Horário",
-        #             "Moeda"
-        #         )
-        #
-        #         colunas_do_ativo_encontrado = st.columns(
-        #             (
-        #                 1, 3, 3
-        #             )
-        #         )
-        #
-        #         primeiro = True
-        #         for ativo_encontrado in resultados_de_busca_de_semelhantes:
-        #             for key, col, nome_coluna in zip(
-        #                 ativo_encontrado.keys(),
-        #                 colunas_do_ativo_encontrado,
-        #                 nome_das_colunas
-        #             ):
-        #                 with col:
-        #                     if primeiro:
-        #                         st.subheader(
-        #                             nome_coluna
-        #                         )
-        #
-        #                     st.write(
-        #                         ativo_encontrado[
-        #                             key
-        #                         ]
-        #                     )
-        #             primeiro = False
-        #
-        #     else:
-        #         st.write(
-        #             "Não há ativos semelhantes à este nome."
-        #         )
-        #
-        # except Exception as error:
-        #     st.error(
-        #         f"Obtive {error}"
-        #     )
+        try:
+            resultados_de_busca_de_semelhantes = buscar_ativos_semelhantes(
+                nome_dado_pelo_usuario,
+                chaves
+            )
+
+            st.write(
+                resultados_de_busca_de_semelhantes
+            )
+
+            if len(
+                resultados_de_busca_de_semelhantes
+            ) > 0:
+
+                nome_das_colunas = (
+                    "Código",
+                    "Nome",
+                    "Tipo",
+                    "Região",
+                    "Abertura",
+                    "Fechamento",
+                    "Fuso Horário",
+                    "Moeda"
+                )
+
+            else:
+                st.write(
+                    "Não há ativos semelhantes à este nome."
+                )
+
+        except Exception as error:
+            st.error(
+                f"Obtive: {error}"
+            )
--- Get_Trading_Data.py
+++ Get_Trading_Data.py
@@ -1,26 +1,64 @@
 import requests as re
-from pprint import pprint
 import streamlit as st
+
 
 def obter_chave(
         nome_do_arquivo_que_contem_chave: str
-) -> str:
+) -> list[list[str] | int]:
     """
     Descrição:
-        Obtém chave da API do usuário a partir de um arquivo .txt
+        Obtém chaves da API do usuário a partir de um arquivo .txt
         que a contém.
 
-        Isso é feito para medidas de segurança.
-    """
+        Foi feito desta maneira para contar o acesso limitado à API.
 
-    chave = "None"
+        Isso é feito para medidas de segurança.
+
+    """
 
     with open(
             nome_do_arquivo_que_contem_chave,
             "r"
     ) as arq:
-        chave = arq.read()
+        lista_de_chaves = arq.readlines()
 
-    return chave
+        lista_de_chaves[0] = lista_de_chaves[0].replace("\n", "")
+
+    return [
+        lista_de_chaves,
+        0
+    ]
 
 
+chaves = obter_chave(
+    r"C:\Users\deyvi\Documents\ImperioPy\Aplicativos\Automação\Banco_e_Visualizador_De_Ativos\CHAVE.txt"
+)
+
+
+def buscar_ativos_semelhantes(
+        chute_do_nome_do_ativo: str,
+        chave: list[list[str] | int]
+) -> list:
+    """
+    Descrição:
+        Obtém lista de possíveis ativos a
+        partir do chute do usuário.
+    """
+    st.write(f"Vendo chaves: {chave}")
+    resultados = re.get(
+        f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={chute_do_nome_do_ativo}&apikey={chave[0][chave[1]]}"
+    ).json()
+
+    if "Information" in resultados:
+        # Chegamos no limite desta chave.
+
+        chave[1] += 1
+
+        return buscar_ativos_semelhantes(
+            chute_do_nome_do_ativo,
+            chave
+        ) if len(chave[0]) != chave[1] else st.error(
+            f"Atingiu-se o limite de todas as chaves. {resultados}"
+        )
+
+    return resultados["bestMatches"]
