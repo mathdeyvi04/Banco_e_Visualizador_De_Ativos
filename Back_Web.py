ebf382f1e4ed79355bd8fc6edf23f0fd2071b2c5
MathDeyvi <deyvisson.moreno@ime.eb.br>
2025-02-03 11:02:34 -0300

    BUILD: Aprofundamento da Interface do Usuário
    ADD: Uso de resultado teste da API, para impedir que à usemos deliberadamente.

--- Back_Web.py
+++ Back_Web.py
@@ -13,6 +13,17 @@ def obter_ativos_cadastrados() -> list[str]:
     ]
 
 
+def selecionar_atributos_do_ativo(
+        opcao_escolhida: str | None
+) -> None:
+    """
+    Descrição:
+        
+    """
+
+    st.write(opcao_escolhida)
+
+
 def cadastrar_novo_ativo():
     """
     Descrição:
@@ -24,24 +35,36 @@ def cadastrar_novo_ativo():
         "Cadastrando Novo Ativo"
     )
 
+    st.divider()
+
     nome_dado_pelo_usuario = st.text_input(
-        label="Insira o possível símbolo do ativo"
+        label="Insira o possível símbolo do ativo",
+        help="Coloque o código que você acredita que seja do ativo desejado."
     )
 
     if nome_dado_pelo_usuario != "":
 
         try:
-            resultados_de_busca_de_semelhantes = buscar_ativos_semelhantes(
-                nome_dado_pelo_usuario,
-                chaves
-            )
+            if "chaves" not in st.session_state:
+                st.session_state[
+                    "chaves"
+                ] = obter_chave(
+                    NOME_DO_ARQUIVO_DE_CHAVES
+                )
 
-            st.write(
-                resultados_de_busca_de_semelhantes
+            st.session_state[
+                "Resultados_De_Busca_De_Semelhantes"
+            ] = buscar_ativos_semelhantes(
+                nome_dado_pelo_usuario,
+                st.session_state[
+                    "chaves"
+                ]
             )
 
             if len(
-                resultados_de_busca_de_semelhantes
+                    st.session_state[
+                        "Resultados_De_Busca_De_Semelhantes"
+                    ]
             ) > 0:
 
                 nome_das_colunas = (
@@ -54,6 +77,66 @@ def cadastrar_novo_ativo():
                     "Fuso Horário",
                     "Moeda"
                 )
+
+                nome_das_chaves = st.session_state[
+                    "Resultados_De_Busca_De_Semelhantes"
+                ][0].keys()
+
+                resultados_em_dataframe = DataFrame(
+                    {
+                        nome_coluna: [
+                            ativo_encontrado[
+                                nome_chave
+                            ] for ativo_encontrado in st.session_state[
+                                "Resultados_De_Busca_De_Semelhantes"
+                            ]
+                        ] for nome_coluna, nome_chave in zip(
+                            nome_das_colunas,
+                            nome_das_chaves
+                        )
+                    }
+                ).set_index(
+                    nome_das_colunas[0],
+                    drop=True
+                )
+
+                col1, col2 = st.columns(
+                    (
+                        6, 1
+                    )
+                )
+
+                with col1:
+
+                    st.dataframe(
+                        resultados_em_dataframe,
+                        width=1400
+                    )
+
+                opcao_escolhida = None
+                with col2:
+                    st.write("")
+
+                    opcao_escolhida = st.radio(
+                        label="Opções",
+                        options=[
+                            ativo_encontrado["1. symbol"][:5] for ativo_encontrado in st.session_state[
+                                "Resultados_De_Busca_De_Semelhantes"
+                            ]
+                        ],
+                        captions=[
+                            # Para adicionar espaço.
+                            "" for _ in st.session_state[
+                                "Resultados_De_Busca_De_Semelhantes"
+                            ]
+                        ],
+                        index=None,
+                    )
+
+                if opcao_escolhida:
+                    selecionar_atributos_do_ativo(
+                        opcao_escolhida
+                    )
 
             else:
                 st.write(
--- Get_Trading_Data.py
+++ Get_Trading_Data.py
@@ -1,5 +1,13 @@
 import requests as re
 import streamlit as st
+from pandas import DataFrame
+from pprint import pprint
+
+st.session_state[
+    "Quantas Vezes Usamos API"
+] = 0
+
+NOME_DO_ARQUIVO_DE_CHAVES = r'C:\Users\deyvi\Documents\ImperioPy\Aplicativos\Automação\CHAVE.txt'
 
 
 def obter_chave(
@@ -10,10 +18,9 @@ def obter_chave(
         Obtém chaves da API do usuário a partir de um arquivo .txt
         que a contém.
 
-        Foi feito desta maneira para contar o acesso limitado à API.
+        Utilizamos inúmeras chaves para contornar o acesso limitado à API.
 
-        Isso é feito para medidas de segurança.
-
+        Utilizamos
     """
 
     with open(
@@ -22,7 +29,20 @@ def obter_chave(
     ) as arq:
         lista_de_chaves = arq.readlines()
 
-        lista_de_chaves[0] = lista_de_chaves[0].replace("\n", "")
+        for index in range(
+                0,
+                len(
+                    lista_de_chaves
+                )
+        ):
+            lista_de_chaves[
+                index
+            ] = lista_de_chaves[
+                index
+            ].replace(
+                "\n",
+                ""
+            )
 
     return [
         lista_de_chaves,
@@ -30,29 +50,63 @@ def obter_chave(
     ]
 
 
-chaves = obter_chave(
-    r"C:\Users\deyvi\Documents\ImperioPy\Aplicativos\Automação\Banco_e_Visualizador_De_Ativos\CHAVE.txt"
+st.session_state[
+    "chaves"
+] = obter_chave(
+    NOME_DO_ARQUIVO_DE_CHAVES
 )
 
 
 def buscar_ativos_semelhantes(
         chute_do_nome_do_ativo: str,
         chave: list[list[str] | int]
-) -> list:
+) -> list[dict[str, str]]:
     """
     Descrição:
         Obtém lista de possíveis ativos a
         partir do chute do usuário.
     """
-    st.write(f"Vendo chaves: {chave}")
-    resultados = re.get(
-        f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={chute_do_nome_do_ativo}&apikey={chave[0][chave[1]]}"
-    ).json()
+
+    resultados = {
+        "bestMatches": [
+            # Para não abusar da API
+            {
+                "1. symbol": f"AW {i}",
+                "2. name": "A&W Revenue Royalties Income Fund",
+                "3. type": "Equity",
+                "4. region": "Toronto",
+                "5. marketOpen": "09:30",
+                "6. marketClose": "16:00",
+                "7. timezone": "UTC-05",
+                "8. currency": "CAD",
+                "9. matchScore": "0.6800"
+            } for i in range(0, 10)
+        ]
+    }
+
+    # resultados = re.get(
+    #     f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={chute_do_nome_do_ativo}&apikey={chave[0][chave[1]]}"
+    # ).json()
+
+    if "Quantas Vezes Usamos API" not in st.session_state:
+        st.session_state[
+            "Quantas Vezes Usamos API"
+        ] = 0
+
+    st.session_state[
+        "Quantas Vezes Usamos API"
+    ] += 1
+
+    st.info(f"Hop vim: {st.session_state['Quantas Vezes Usamos API']}.")
 
     if "Information" in resultados:
         # Chegamos no limite desta chave.
 
         chave[1] += 1
+
+        st.warning(
+            f"Trocando para nova chave, total de usos: {st.session_state['Quantas Vezes Usamos API']}."
+        )
 
         return buscar_ativos_semelhantes(
             chute_do_nome_do_ativo,
@@ -62,3 +116,4 @@ def buscar_ativos_semelhantes(
         )
 
     return resultados["bestMatches"]
+
